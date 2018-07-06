cf2f756a1e0a75fb1dc350db7412d1f250114ac9
Katie Broida <katie.broida@upstatement.com>
2018-07-06 11:40:58 -0400

    Add custom Mobile Story post type and enable REST API for gutenberg

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+
+define('MOBILE_THEME_URL', get_template_directory_uri());
+define('MOBILE_THEME_PATH', dirname(__FILE__) . '/');
+define('MOBILE_DOMAIN', get_site_url());
+define('MOBILE_SITE_NAME', get_bloginfo('name'));
+include MOBILE_THEME_PATH . 'vendor/autoload.php';
+
+/**
+ * Custom post types and taxonomies.
+ */
+function mobile_architecture()
+{
+  // Post Types
+  require_once MOBILE_THEME_PATH . 'lib/custom_post_types/MobileStories.php';
+}
+
+add_action('init', 'mobile_architecture', 0);
